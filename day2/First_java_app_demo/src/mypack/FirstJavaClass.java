82420ff071ff428f0cb8133aa4b37eeadd6ae028
U2jj19 <u2jj.19@gmail.com>
2021-07-30 14:15:07 +0530

    day2

---
+++ day2/First_java_app_demo/src/mypack/FirstJavaClass.java
@@ -0,0 +1,17 @@
+package mypack;
+
+public class FirstJavaClass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.print("hola");
+		
+		System.out.println("bye");
+		System.out.print("hii");
+		
+		
+
+
+	}
+
+}
